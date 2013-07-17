4fbb6f19e1dbeafaf1a5941d69492f2d6d2f7f5e
BranimirE <brani.espinoza@gmail.com>
2013-07-17 01:04:12 -0400

    added modpow

--- main.cpp
+++ main.cpp
@@ -1,9 +1,36 @@
+#include <iostream>
 #include <cstdio>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
 
 using namespace std;
+long long int pow(int b, int e){
+	if(!e) return 1;
+
+	long long int ans = pow(b, e>>1);
+	ans *= ans;
+
+	if(e&1)
+		ans *= b;
+	return ans;
+}
+
+unsigned long long int modpow(long long int b, int e, int m){
+	if(!e) return 1;
+
+	unsigned long long int ans = pow(b, e>>1);
+	ans = (ans * ans) % m;
+
+	if(e&1)
+		ans = (ans * b) % m;
+	return ans;
+}
 
 int main(){
-	//comentario
+	freopen("entrada.in", "r", stdin);
 
 	return 0;
 }
