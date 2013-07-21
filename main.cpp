ca7c714cc11c5cd9b48ba082d7050760ec2490ba
BranimirE <brani.espinoza@gmail.com>
2013-07-20 21:46:10 -0400

    Evitar desborde en multiplicacion, al utilizar exponenciacion rapida

--- main.cpp
+++ main.cpp
@@ -21,16 +21,22 @@ long long int pow(int b, int e){
 unsigned long long int modpow(long long int b, int e, int m){
 	if(!e) return 1;
 
-	unsigned long long int ans = pow(b, e>>1);
-	ans = (ans * ans) % m;
+	unsigned long long int ans = modpow(b, e>>1, m);
+	ans = (ans * ans) % m;// cuidado con desborde en la multiplicacion
 
 	if(e&1)
 		ans = (ans * b) % m;
 	return ans;
 }
+//podemos utilizar una idea similar para evitar el desborde en la multiplicacion
+int multi(long long a, long long b, long long mod){
+	if(b == 0) return 0;
+	long long int ans = (multi( a, b / 2, mod ) * 2) % mod;
+	if( b % 2 == 1 ) ans = (ans + a) % mod;
+	return ans;
+}
 
 int main(){
 	freopen("entrada.in", "r", stdin);
-
 	return 0;
 }
