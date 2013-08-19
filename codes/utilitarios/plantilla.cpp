7aa6e2ef0879c407ead76a4e6574d84d20180437
starsaminf <starsaminf@gmail.com>
2013-08-18 18:47:26 -0700

    Create plantilla.cpp

---
+++ codes/utilitarios/plantilla.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <cassert>
+#include <sstream>
+#include <fstream>
+#include <cstdlib>
+#include <cstring>
+#include <utility>
+#include <complex>
+#include <string>
+#include <cctype>
+#include <cstdio>
+#include <vector>
+#include <bitset>
+#include <stack>
+#include <queue>
+#include <cmath>
+#include <deque>
+#include <list>
+#include <set>
+#include <map>
+
+#define ll long long
+#define lld long long int
+#define sc scanf
+#define pf printf
+#define pi 2*acos(0.0)
+#define f first
+#define s second
+#define sz size()
+#define mp make_pair
+#define r(input) freopen("2966.in","r",stdin)
+#define w(output) freopen("output.txt","w",stdout)
+#define maxall(v) *max_element(v.begin(),v.end())
+#define minall(v) *min_element(v.begin(),v.end())
+#define Sort(v) sort(v.begin(),v.end())
+#define un(v) Sort(v), v.erase(unique(v.begin(),v.end()),v.end())
+#define clr(a)  memset(a,0,sizeof(a))
+#define pb push_back
+#define lim 100000 
