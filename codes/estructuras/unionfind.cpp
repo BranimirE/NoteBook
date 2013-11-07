1e3ac2a760a111ae8cdcdab527c8d4c783c4b9aa
BranimirE <brani.espinoza@gmail.com>
2013-11-07 12:07:45 -0400

    unionfind.cpp

--- codes/estructuras/unionfind.cpp
+++ codes/estructuras/unionfind.cpp
@@ -1,22 +1,38 @@
-#define MAX 100010
-
-using namespace std;
-
-int padre[ MAX ];
-int rango[ MAX ];
-
-void MakeSet( int n ){for(int i = 0 ; i <= n ; ++i )padre[ i ] = i, rango[ i ] = 0;}
-int Find(int x){return (padre[x]==x?x:padre[x]=Find(padre[x]));}
-bool sameComponent(int x, int y){ return Find(x) == Find(y);}
-
-void Union( int x , int y ){
-    int xRoot = Find( x );
-    int yRoot = Find( y );
-    if( rango[ xRoot ] > rango[ yRoot ] )
-        padre[ yRoot ] = xRoot;            
-    else{                   
-        padre[ xRoot ] = yRoot;
-        if( rango[ xRoot ] == rango[ yRoot ] )
-            rango[ yRoot ]++;
+struct UnionFind{
+  int p[MAX], r[MAX], setSize[MAX];
+  int n, numSets;
+  UnionFind(int _N){
+    reset(_N);
+  }
+  void reset(int N){
+    n = N;
+    numSets = n;
+    for(int i = 0; i <= n; i++){
+      p[i] = i;
+      r[i] = 0;
+      setSize[i] = 1;
     }
-}
+  }
+  int Find(int i){return (p[i] == i)?i:(p[i] = Find(p[i]));}
+  bool sameComponent(int i, int j){return Find(i) == Find(j);}
+  void Union(int i, int j){
+    if(!sameComponent(i, j)){
+      numSets--;
+      int x = Find(i), y = Find(j);
+      if(r[x] > r[y]){
+        p[y] = x;
+        setSize[x] += setSize[y];
+      } else                   {
+        p[x] = y;
+        setSize[y] += setSize[x];
+        if (r[x] == r[y]) r[y]++;
+      }
+    }
+  }
+  int numDisjointSets(){
+    return numSets;
+  }
+  int sizeOfSet(int i){
+    return setSize[Find(i)];
+  }
+};
