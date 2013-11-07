aaa857ae66fe79d05364f67ad11cf7750fbd61f7
starsaminf <starsaminf@gmail.com>
2013-11-06 18:42:20 -0800

    Create manacher2.cpp

---
+++ codes/cadenas/manacher2.cpp
@@ -0,0 +1,26 @@
+#define MAXN 600005
+char str[MAXN];
+int rad[MAXN];
+int n;
+/*
+EN rad se guarda el tamaño del palindromo
+str[0]='$';         // si o si 
+scanf("%s", str+1); // puntero para iniciar desde 1
+n=strlen(str+1);
+*/
+void Manacher() {
+    int i, j, k;
+    i = 1;
+    j = 0;
+    while (i <= n) {
+        while (str[i - j] == str[i + j + 1] && i - j > 0 && i + j + 1 <= n) j++;
+        rad[i] = j;
+        k = 1;
+        while (k <= rad[i] && rad[i - k] != rad[i] - k) {
+            rad[i + k] = min(rad[i - k], rad[i] - k);
+            k++;
+        }
+        i = i + k;
+        j = max(j - k, 0);
+    }
+}
